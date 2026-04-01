25181b3527c7c43121141ffde4af409b936bc48e
subh <subhamsahoo0p90@gmail.com>
2026-04-01 15:40:18 +0530

    Added env config and improved backend security

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,20 @@
 import express from "express";
 import { createServer } from "node:http";
-
 import { Server } from "socket.io";
-
 import mongoose from "mongoose";
 import { connectToSocket } from "./controllers/socketManager.js";
-
 import cors from "cors";
 import userRoutes from "./routes/users.routes.js";
+import dotenv from "dotenv";
+
+dotenv.config(); // ✅ importantnn
 
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
+app.set("port", process.env.PORT || 8000);
 
-app.set("port", (process.env.PORT || 8000))
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
@@ -22,18 +22,18 @@ app.use(express.urlencoded({ limit: "40kb", extended: true }));
 app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
-    app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://subh_3000:subham3000@zoom.or6qk5z.mongodb.net/")
+    try {
+        const connectionDb = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
-    server.listen(app.get("port"), () => {
-        console.log("LISTENIN ON PORT 8000")
-    });
+        console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`);
 
+        server.listen(app.get("port"), () => {
+            console.log(`LISTENING ON PORT ${app.get("port")}`);
+        });
 
-
-}
-
-
+    } catch (error) {
+        console.log("DB ERROR:", error);
+    }
+};
 
 start();
